55437654468d40f0960e5ed16ca4c4a177b05624
Ivanlovening <1073477805@qq.com>
2018-09-14 10:35:49 +0800

    Update response.php
    删除appid appkey

--- response.php
+++ response.php
@@ -1,11 +1,11 @@
 <?php
 
 require './WeChat.class.php';
-define('APPID','wxc46d6f0b058b3f04');
-define('APPSECRET','06ddde3f8edb345eb0ee71609b82c1d3');
-define('TOKEN','yinfei_weixin');
+define('APPID','');
+define('APPSECRET','');
+define('TOKEN','');
 //
-define('APPKEY','07a14173c45e4d58d94a0781953bdd03');
+define('APPKEY','');
 $wechat = new WeChat(APPID,APPSECRET,TOKEN,APPKEY);
 //$access_token = $wechat->getAccessToken();
 //$qrcode = $wechat->getQRCode(124);
